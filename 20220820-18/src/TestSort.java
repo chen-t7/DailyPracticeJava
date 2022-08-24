555289e2c16e32cc99401e45c1d35a452a23c138
chen-t7 <chenyu5w@163.com>
2022-08-24 22:44:05 +0800

    排序

--- 20220820-18/src/TestSort.java
+++ 20220820-18/src/TestSort.java
@@ -151,7 +151,7 @@ public class TestSort {
         System.out.println(end - start);
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         test1(100_0000);
         test2(100_0000);
     }
@@ -359,29 +359,188 @@ public class TestSort {
         }
     }
 
+    public static int[] mergeArray(int[] array1, int[] array2) {
+        if (array1 == null) {
+            return array2;
+        }
+        if (array2 == null) {
+            return array1;
+        }
+        int[] ret = new int[array1.length + array2.length];
+        int index1 = 0;
+        int index2 = 0;
+        int i = 0;
+        while (index1 < array1.length && index2 < array2.length) {
+            if (array1[index1] <= array2[index2]) {
+                ret[i++] = array1[index1++];
+            }
+            if (array2[index2] <= array1[index1]) {
+                ret[i++] = array2[index2++];
+            }
+        }
+
+        while (index1 < array1.length) {
+            ret[i++] = array1[index1++];
+        }
+        while (index2 < array2.length) {
+            ret[i++] = array2[index2++];
+        }
+        return ret;
+    }
+
+    /**
+     * 归并排序
+     * 时间复杂度：O(N*logN)
+     * 空间复杂度：O(N)
+     * 稳定性：稳定的排序，如果if (array[index1] <= array[index2]) 不取等号，就是不稳定的
+     * @param array
+     */
+    public static void mergeSort1(int[] array) {
+        mergeSortInternal(array, 0, array.length-1);
+    }
+
+    private static void mergeSortInternal(int[] array, int low, int high) {
+        if (low >= high) {
+            return ;
+        }
+        int mid = low + ((high - low) >> 1);
+
+        mergeSortInternal(array, low, mid);
+        mergeSortInternal(array,mid+1, high);
+        merge(array, low, mid, high);
+    }
+
+    private static void merge(int[] array, int low, int mid, int high) {
+        int[] ret = new int[high-low+1];
+        int i = 0;
+        int index1 = low;
+        int e1 = mid;
+        int index2 = mid + 1;
+        int e2 = high;
+        while (index1 <= e1 && index2 <= e2) {
+            if (array[index1] <= array[index2]) {
+                ret[i++] = array[index1++];
+            } else {
+                ret[i++] = array[index2++];
+            }
+        }
+
+        while (index1 <= e1) {
+            ret[i++] = array[index1++];
+        }
+        while (index2 <= e2) {
+            ret[i++] = array[index2++];
+        }
+        for (int j = 0; j < ret.length; j++) {
+            array[j+low] = ret[j];
+        }
+    }
+
+    /**
+     * 非递归实现 归并排序
+     * @param array
+     */
+    public static void mergeSort(int[] array) {
+        int nums = 1;//每组的数据个数
+        while (nums < array.length) {
+            //数组每次都要从0开始遍历,确定要归并的区间
+            for (int i = 0; i <array.length ; i += nums*2) {
+                int left = i;
+                int mid = left + nums - 1;
+                if (mid >= array.length) {
+                    mid = array.length-1;
+                }
+                int right = mid + nums;
+                if (right >= array.length) {
+                    right = array.length - 1;
+                }
+                //下标确定之后，进行合并
+                merge(array, left, mid, right);
+            }
+            nums *= 2;
+        }
+    }
+
+    /**
+     * 计数排序
+     *一般适用于 有n个数，数据范围是0~n之间的
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(M),M代表当前数据的范围
+     * 稳定性：当前代码是不稳定的，但是本质是稳定的，可以再借用一个数组标记
+     * @param array
+     */
+    public static void countSort(int[] array) {
+        if (array == null) {
+            return ;
+        }
+        int maxVal = array[0];
+        int minVal = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minVal) {
+                minVal = array[i];
+            }
+            if (array[i] > maxVal) {
+                maxVal = array[i];
+            }
+        }
+        //找到了最大值和最小值
+        //如果数据范围是900~999，只需开辟maxVal-minVal+1个空间
+        int[] count = new int[maxVal-minVal+1];
+        //统计array中，每个数字出现的次数
+        for (int i = 0; i < array.length; i++) {
+            int index = array[i];
+            //为了空间的合理使用，这里需要计数到index-minVal下标
+            count[index-minVal]++;
+        }
+        //说明在计数数组当中，已经把array数组当中每个数据出现的次数已经统计好了
+        int indexArray = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                //要放入i + minVal才能还原真正的数值
+                array[indexArray++] = i + minVal;
+                count[i]--;
+            }
+        }
+    }
+
     public static void main2(String[] args) {
-        int[] array1 = {12,5,18,77,31,10,4,2,86,9,7};
+        int[] array1 = {1,3,6,7,7,8,8,9};
+        int[] array2 = {2,3,4,7,8};
+        int[] array3 = mergeArray(array1, array2);
+        System.out.println(Arrays.toString(array3));
+    }
+
+    public static void main(String[] args) {
+        int[] array1 = {12,5,18,77,31,5,10,4,2,86,9,7};
         shellSort(array1);
         System.out.println(Arrays.toString(array1));
 
-        int[] array2 = {12,5,18,77,31,10,4,2,86,9,7};
+        int[] array2 = {12,5,18,77,31,5,10,4,2,86,9,7};
         insertSort(array2);
         System.out.println(Arrays.toString(array2));
 
-        int[] array3 = {12,5,18,77,31,10,4,2,86,9,7};
+        int[] array3 = {12,5,18,77,31,5,10,4,2,86,9,7};
         selectSort(array3);
         System.out.println(Arrays.toString(array3));
 
-        int[] array4 = {12,5,18,77,31,10,4,2,86,9,7};
+        int[] array4 = {12,5,18,77,31,5,10,4,2,86,9,7};
         selectSort(array4);
         System.out.println(Arrays.toString(array4));
 
-        int[] array5 = {12,5,18,77,31,10,4,2,86,9,7};
+        int[] array5 = {12,5,18,77,31,5,10,4,2,86,9,7};
         bubbleSort2(array5);
         System.out.println(Arrays.toString(array5));
 
-        int[] array6 = {12,5,18,77,31,10,4,2,86,9,7};
+        int[] array6 = {12,5,18,77,31,5,10,4,2,86,9,7};
         quickSort1(array6);
         System.out.println(Arrays.toString(array6));
+
+        int[] array7 = {12,5,18,77,31,5,10,4,2,86,9,7};
+        mergeSort(array7);
+        System.out.println(Arrays.toString(array7));
+
+        int[] array8 = {12,5,18,77,31,5,10,4,2,86,9,7};
+        countSort(array8);
+        System.out.println(Arrays.toString(array8));
     }
 }
